b230e23fec4f0b49074bb5c296d5f567765f1bee
mathantkumar <mathankumar2001@gmail.com>
2026-02-02 01:34:16 +0530

    feat: Disable static export output to enable server-side API routes.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
+  // output: "export", // Disabled to allow Server-Side API Routes
   images: {
     unoptimized: true,
   },
